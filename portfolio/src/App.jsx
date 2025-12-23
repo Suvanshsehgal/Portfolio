56d8a5eb6331062d0e37bce3d4a760963ad4f721
Suvanshsehgal <suvanshsehgal@gmail.com>
2025-12-23 15:17:43 +0530

    Build the structure of our web application

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'   
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Contactus from './components/Contactus'
+import Footer from './components/Footer'
 
-export default function App() {
+function App() {
   return (
-    <div className='text-3xl font-bold underline'>App</div>
+    <>
+    <Navbar/>
+    <HeroSection/>
+    <Projects/>
+    <Skills/>
+    <Experience/>
+    <Contactus/>
+    <Footer/>
+    </>
   )
 }
+
+export default App
